a260bf94daee89dfe1e5ef32157ac87a6a8e46ab
Massayoshii <Massayoshibando@gmail.com>
2026-05-29 10:57:48 -0300

    feat: criacao do service BombaDeCombustivelService

---
+++ posto-combustivel/src/main/java/com/example/posto_combustivel/Service/BombaDeCombustivelService.java
@@ -0,0 +1,39 @@
+package com.example.posto_combustivel.Service;
+
+import com.example.posto_combustivel.Entity.BombasDeCombustivel;
+import com.example.posto_combustivel.Repository.BombasDeCombustivelRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BombaDeCombustivelService {
+
+    private final BombasDeCombustivelRepository bombasDeCombustivelRepository;
+
+    public void criar(BombasDeCombustivel bombasDeCombustivel){
+        bombasDeCombustivelRepository.save(bombasDeCombustivel);
+    }
+
+    private BombasDeCombustivel buscarBombaCombustivelPorId(Long id){
+        return bombasDeCombustivelRepository.findById(id).orElseThrow(() -> new NullPointerException("Bomba de combustivel nao encontrada pelo id " + id));
+    }
+
+    private List<BombasDeCombustivel> buscarBombaCombustivel(){
+        return bombasDeCombustivelRepository.findAll();
+    }
+
+    @Transactional
+    private void deletarBombaDeCombustivel(Long id ){
+        bombasDeCombustivelRepository.deleteById(id);
+    }
+
+    private void alterarBombaCombustivel(Long id , BombasDeCombustivel bombasDeCombustivel){
+        BombasDeCombustivel bomba = buscarBombaCombustivelPorId(id);
+        bombasDeCombustivel.setId(bomba.getId());
+        bombasDeCombustivelRepository.save(bombasDeCombustivel);
+    }
+}

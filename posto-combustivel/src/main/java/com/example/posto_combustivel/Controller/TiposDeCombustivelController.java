b66a89963a0f743b3dbf889f7d696a985cae19d4
Massayoshii <Massayoshibando@gmail.com>
2026-05-29 15:43:57 -0300

    feat: criacao do controller TiposDeCombustivelController

---
+++ posto-combustivel/src/main/java/com/example/posto_combustivel/Controller/TiposDeCombustivelController.java
@@ -0,0 +1,46 @@
+package com.example.posto_combustivel.Controller;
+
+import com.example.posto_combustivel.Entity.TiposDeCombustivel;
+import com.example.posto_combustivel.Service.TipoDeCombustivelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/abastecimento")
+
+public class TiposDeCombustivelController {
+    private final TipoDeCombustivelService tipoDeCombustivelService;
+
+    @PostMapping
+    public ResponseEntity<Void> criar(@RequestBody TiposDeCombustivel tiposDeCombustivel){
+        tipoDeCombustivelService.criar(tiposDeCombustivel);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TiposDeCombustivel>> buscarTiposDeCombustivel(){
+        return ResponseEntity.ok(tipoDeCombustivelService.buscarTipoDeCombustivel());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TiposDeCombustivel> buscarTiposDeCombustivelPorId(@PathVariable (name = "id") Long id){
+        return ResponseEntity.ok(tipoDeCombustivelService.buscarTipoDeCombustivelPorId(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTiposDeCombustivelPorId(@PathVariable (name = "id") Long id){
+        tipoDeCombustivelService.deletarTipoDeCombustivel(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> alterarTipoDeCombustivel(@RequestParam (name = "id") Long id ,@RequestBody TiposDeCombustivel tiposDeCombustivel){
+        tipoDeCombustivelService.alterarTipoDeCombustivel(id,tiposDeCombustivel);
+        return ResponseEntity.accepted().build();
+    }
+
+}
